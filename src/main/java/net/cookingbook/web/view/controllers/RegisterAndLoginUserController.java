ae6d46d3cd3500d538b902b553f88bb5b5ff8fa0
Hristo Panev <hristopanef@gmail.com>
2019-12-16 21:11:36 +0100

    edit RegAndLogingController

--- src/main/java/net/cookingbook/web/view/controllers/RegisterAndLoginUserController.java
+++ src/main/java/net/cookingbook/web/view/controllers/RegisterAndLoginUserController.java
@@ -1,5 +1,6 @@
 package net.cookingbook.web.view.controllers;
 
+import net.cookingbook.validation.user.UserLoginValidator;
 import net.cookingbook.validation.user.UserRegisterValidator;
 import net.cookingbook.web.base.BaseController;
 import net.cookingbook.web.view.models.binding.UserRegisterBindingModel;
@@ -8,8 +9,8 @@ import net.cookingbook.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
@@ -21,18 +22,19 @@ public class RegisterAndLoginUserController extends BaseController {
 
     private final UserService userService;
     private final ModelMapper modelMapper;
-    private UserRegisterValidator userRegisterValidator;
+    private final UserRegisterValidator userRegisterValidator;
+    private final UserLoginValidator userLoginValidator;
 
     @Autowired
-    public RegisterAndLoginUserController(UserService userService, ModelMapper modelMapper, UserRegisterValidator userRegisterValidator) {
+    public RegisterAndLoginUserController(UserService userService, ModelMapper modelMapper, UserRegisterValidator userRegisterValidator, UserLoginValidator userLoginValidator) {
         super(userService);
         this.userService = userService;
         this.modelMapper = modelMapper;
         this.userRegisterValidator = userRegisterValidator;
+        this.userLoginValidator = userLoginValidator;
     }
 
     @PostMapping("/register")
-    @PreAuthorize("isAnonymous()")
     public ModelAndView registerConfirm(@ModelAttribute(name = "model") UserRegisterBindingModel model,  BindingResult bindingResult) {
 
         this.userRegisterValidator.validate(model, bindingResult);
@@ -49,14 +51,12 @@ public class RegisterAndLoginUserController extends BaseController {
     }
 
     @GetMapping("/user-register")
-    @PreAuthorize("isAnonymous()")
     public ModelAndView userRegister(ModelAndView modelAndView, @ModelAttribute(name = "model") UserRegisterBindingModel model) {
         modelAndView.addObject("model", model);
-        return super.view("/users/user-register", modelAndView);
+        return super.view("users/user-register", modelAndView);
     }
 
     @PostMapping("/user-register")
-    @PreAuthorize("isAnonymous()")
     public ModelAndView registerIncorrect(ModelAndView modelAndView, @ModelAttribute(name = "model") UserRegisterBindingModel model, BindingResult bindingResult) {
 
         this.userRegisterValidator.validate(model, bindingResult);
@@ -76,7 +76,6 @@ public class RegisterAndLoginUserController extends BaseController {
 
 
     @GetMapping("/login")
-    @PreAuthorize("isAnonymous()")
     public ModelAndView login() {
         return super.view("/users/login");
     }
